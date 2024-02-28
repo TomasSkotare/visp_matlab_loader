1b1309710ace2407637ab11077c20e1faac1f4b5
Tomas Skotare <tomas.skotare@umu.se>
2024-02-28 16:00:30 +0100

    Added a trivial matlab library for testing

---
+++ matlab/libraries/get_next_thousand/getnextthousand.m
@@ -0,0 +1,3 @@
+function nextNumbers = getnextthousand(inputNumber)
+    nextNumbers = (inputNumber+1):(inputNumber+1000);
+end
---
+++ matlab/libraries/get_next_thousand/getnexttwothousand.m
@@ -0,0 +1,3 @@
+function nextNumbers = getnexttwothousand(inputNumber)
+    nextNumbers = (inputNumber+1):(inputNumber+2000);
+end
