830f31559776ffa48e6417b4debe9166bd4edd62
Tomas Skotare <tomas.skotare@umu.se>
2024-03-06 13:57:22 +0100

    Added a test class

--- notebooks/py_notebooks/Compile_And_Test_Projects.py
+++ notebooks/py_notebooks/Compile_And_Test_Projects.py
@@ -45,46 +45,26 @@ for project_path, exit_status, message in results:
         print(f'Compiled {project_name} successfully')
 
 # %%
-from visp_matlab_loader.find_compiled_projects import CompiledProjectFinder
-
-compiled_projects = CompiledProjectFinder(MATLAB_COMPILED_PATH)
-for project in compiled_projects.found_projects:
-    print(project)
 
 
+
+# %% [markdown]
+# # Test compiled projects:
 
 # %%
-# Now we have the compiled projects, and can check if any of them have any test data
 
+import sys
+import logging
+
+sys.path.append('..')
+
+from visp_matlab_loader.find_compiled_projects import CompiledProjectFinder
 from visp_matlab_loader.execute.matlab_execution_result import MatlabExecutionResult
+from visp_matlab_loader.test.project_tests import TestMatlabProject
 
-def execute_projects(compiled_projects: CompiledProjectFinder) -> None:
-    for project in compiled_projects.found_projects:
-        print(f'Project {project.name}, test directory: {project.test_case_directory}')
+compiled_projects = CompiledProjectFinder(MATLAB_COMPILED_PATH, verbose=False)
 
-        if not project.test_case_files:
-            print('\tProject has no test data, skipping...')
-            continue
-
-        print('Project has test data\nTest case files:')
-        for test_case in project.test_case_files:
-            execution_result = MatlabExecutionResult.from_json(file=test_case)
-            print(f'\t{test_case}\nExecution result of function: {execution_result.function_name}')
-
-            matlab_function = project.functions.get(execution_result.function_name)
-            if not matlab_function:
-                print('Function not found')
-                continue
-
-            matlab_function.override_output_count(len(execution_result.outputs))
-            print(f'Found function: {matlab_function}\nExecuting...')
-            new_result = matlab_function.execute(*execution_result.inputs)
-
-            if not new_result.compare_results(execution_result, verbose=True):
-                print('Results do not match')
-            else:
-                print('Results match!')
-
-execute_projects(compiled_projects)
-            
-            
+for compiled_project in compiled_projects.found_projects:
+    tester = TestMatlabProject(compiled_project)
+    with tester.temporary_log_level('DEBUG'):  
+        tester.test_project()
---
+++ visp_matlab_loader/test/project_tests.py
@@ -0,0 +1,66 @@
+
+
+from contextlib import contextmanager
+from visp_matlab_loader.execute.matlab_execution_result import MatlabExecutionResult
+from visp_matlab_loader.project.matlab_project import MatlabProject
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+handler = logging.StreamHandler() 
+handler.setLevel(logging.DEBUG)
+logger.addHandler(handler)
+
+class TestMatlabProject:
+    
+    @property
+    def test_case_files(self) -> list[str]:
+        return self.project.test_case_files
+    
+    def __init__(self, project: MatlabProject) -> None:
+        self.project = project
+        
+    def test_project(self) -> list[tuple[str, MatlabExecutionResult, MatlabExecutionResult]]:
+        project = self.project
+        
+        if not project.test_case_files:
+            logger.warning(f'Project {project.name} has no test data, skipping...')
+            return []
+        logger.info(f'Project {project.name}, test directory: {project.test_case_directory}')        
+        logger.debug(f'Available functions: {project.functions.keys()}')
+
+
+
+        logger.info('Project has test data\nTest case files:')
+        test_results = []
+        for test_case in project.test_case_files:
+            test_case_execution = MatlabExecutionResult.from_json(file=test_case)
+            logger.info(f'\t{test_case}\nExecution result of function: {test_case_execution.function_name}')
+
+            matlab_function = project.functions.get(test_case_execution.function_name)
+            if not matlab_function:
+                logger.warning(f'Function {test_case_execution.function_name} not found in the project {project.name}! Skipping...')
+                continue
+
+            matlab_function.override_output_count(len(test_case_execution.outputs))
+            logger.info(f'Found function: {matlab_function}\nExecuting...')
+            new_result = matlab_function.execute(*test_case_execution.inputs)
+
+            if not new_result.compare_results(test_case_execution, verbose=True):
+                logger.warning('Results do not match')
+            else:
+                logger.info('Results match!')
+            test_results.append((test_case, test_case_execution, new_result))
+        
+        return test_results
+    
+    @contextmanager
+    def temporary_log_level(self, level):
+        old_level = logger.level
+        logger.setLevel(level)
+        try:
+            yield
+        finally:
+            logger.setLevel(old_level)
+            
